e287e8d108d4452c2f9a10a5aa010beec015c8c7
Ruslan <karuslan@inbox.ru>
2022-02-13 13:13:06 +0300

    Добавьте файлы проекта.

---
+++ Lesson5-1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+//Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл.
+string dataSet = Console.ReadLine();
+string filename = "dataSet.txt";
+File.WriteAllText(filename, dataSet);
