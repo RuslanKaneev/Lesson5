2f52586a47262489c3f53996ea3d47e4f7d839e2
Ruslan <karuslan@inbox.ru>
2022-02-13 14:15:49 +0300

    Задание 2

---
+++ Lesson5-1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+Console.WriteLine("Введите строку");
+string dataSet = Console.ReadLine();
+string filename = "dataSet.txt";
+File.WriteAllText(filename, dataSet);
---
+++ Lesson5-2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Написать программу, которая при старте дописывает текущее время в файл «startup.txt».
+using System.IO;
+string currentTime = Convert.ToString(DateTime.Now.ToString("HH:mm:ss"));
+string filename = "startup.txt";
+File.WriteAllText(filename, currentTime);
